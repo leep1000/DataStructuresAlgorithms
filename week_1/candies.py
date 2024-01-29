213129b51af96d05f4f80db3410c9bc904488e1f
Lee Palmer <leepalmerformal@gmail.com>
2024-01-29 00:19:47 +0000

    completed candies

---
+++ week_1/candies.py
@@ -0,0 +1,12 @@
+def count(a, b, c):
+    if a < b:
+        return c // a
+    else: 
+        return c // b
+        
+
+if __name__ == "__main__":
+    print(count(3, 4, 11)) # 3
+    print(count(5, 1, 100)) # 100
+    print(count(2, 3, 1)) # 0
+    print(count(2, 3, 9)) # 4
