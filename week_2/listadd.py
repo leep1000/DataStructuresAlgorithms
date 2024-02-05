1956d4ba012cb94d39cad30450e8a99a353319e3
Lee Palmer <leepalmerformal@gmail.com>
2024-02-05 00:05:06 +0000

    list add

---
+++ week_2/listadd.py
@@ -0,0 +1,12 @@
+def create(t, x):
+    new_list = t[:]
+    for i in range(len(new_list)):
+        new_list[i] = new_list[i] + x
+
+    return new_list 
+
+
+if __name__ == "__main__":
+    print(create([1,2,3],1)) # [2,3,4]
+    print(create([1,1,1,1,1],4)) # [5,5,5,5,5]
+    print(create([0],10**9)) # [1000000000]
